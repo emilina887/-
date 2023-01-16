f7b6656f9fc59976f1e1aead6b6f23f167c918c6
emilina887 <emilinab9@gmail.com>
2023-01-17 00:08:38 +0200

    3.11

--- 13.01lesson/main.py
+++ 13.01lesson/main.py
@@ -1,25 +1,26 @@
-result = []
-
-
 def divider(a, b):
+    if type == str:
+        a = int(a)
+    if b == 0:
+        b = 1
     try:
-        a / b
+        c = a / b
+        return c
     except ZeroDivisionError:
         print('division by zero')
-    finally:
-        if type(a) == str:
-            a = int(a)
-    if a < b:
-        try:
-            a = a + b
-        except ValueError:
-            print('a < b')
-    if b > 100:
-        raise IndexError
-    return a / b
 
 
-data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8: 4}
+if a < b:
+    try:
+        a = a + b
+    except ValueError:
+        print('A < B')
+if b > 100:
+    raise IndexError
+
+result = []
+
+data = {10: 2, 2: 5, "123": 4, 18: 0, 1: 15, 8: 4}
 for key in data:
     res = divider(key, data[key])
     result.append(res)
